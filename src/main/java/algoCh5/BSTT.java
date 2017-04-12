9c56afba2305e7f819e86ff24aa2371aa4e09711
Andrew Valancius <valancal199@potsdam.edu>
2017-04-11 22:10:18 -0400

    Problem #5 is implemented

--- src/main/java/algoCh5/BSTT.java
+++ src/main/java/algoCh5/BSTT.java
@@ -13,24 +13,58 @@ import java.lang.Comparable;
 class BSTT <Key extends Comparable<? super Key>, E>
              extends BST <Key, E> {
 
-
+    protected BSTTNode<Key,E> root;
+    
+    /** Insert a record into the tree.
+    * @param k Key value of the record.
+    * @param e The record to insert.
+    *  
+    */
+    public void insert(Key k, E e) {
+        root = inserthelp(root, k, e, null, null);
+        nodecount++;
+    }
     
     // override inserthelp()
-    protected BSTNode<Key,E> inserthelp(BSTNode<Key,E> rt, Key k, E e) {
+    protected BSTTNode<Key,E> inserthelp(BSTTNode<Key,E> rt, Key k, E e, BSTTNode<Key,E> p, String direction) {
         if (rt == null) 
-           // TODO change this constructor to use the new threaded one
-           return new BSTTNode<Key,E>(k, e);
+            
+            if (direction.equals("LEFT")) {
+                return new BSTTNode<Key,E>(k, e, p, p);
+            } else if (direction.equals("RIGHT")) {
+                if (p.getThread() == null) {
+                    BSTTNode<Key, E> me = new BSTTNode<Key,E>(k, e, p, null);
+                    p.setThread(me);
+                    return me;
+                } else {
+                    BSTTNode<Key, E> me = new BSTTNode<Key,E>(k, e, p, p.parent());
+                    p.setThread(me);
+                    return me;
+                }
+                
+            }
+            
+            
+            
+           
         if (rt.key().compareTo(k) > 0)
-           rt.setLeft(inserthelp(rt.left(), k, e));
+           rt.setLeft(inserthelp(rt.left(), k, e, rt, "LEFT"));
         else
-          rt.setRight(inserthelp(rt.right(), k, e));
+          rt.setRight(inserthelp(rt.right(), k, e, rt, "RIGHT"));
         return rt;
     }
     
     
     
     // override printhelp()
-
+    protected void printhelp(BSTTNode<Key,E> rt) {
+        BSTTNode<Key,E> curr = rt.getThread();
+        while (curr != null) {
+            printVisit(curr.key(), curr.element());
+            curr = curr.getThread();
+        }
+        
+    }
 
 
 }
--- src/main/java/algoCh5/BSTTNode.java
+++ src/main/java/algoCh5/BSTTNode.java
@@ -1,18 +1,57 @@
 package algoCh5;
 
 class BSTTNode<Key,E> extends BSTNode<Key,E> {
+    
+    private BSTTNode<Key,E> left;  // Pointer to left child
+    private BSTTNode<Key,E> right; // Pointer to right child
  
     boolean isThreaded = true;
-    BSTNode<Key, E> thread = null;
+    BSTTNode<Key, E> parent;
+    BSTTNode<Key, E> thread;
     
-    public BSTTNode(Key k, E val) { 
-        this(k, val, null, null, null );
+    public BSTTNode(Key k, E val, BSTTNode<Key,E> p, BSTTNode<Key, E> t) { 
+        this(k, val, null, null, p, t);
     }
     
-    public BSTTNode(Key k, E val, BSTNode<Key,E> l, BSTNode<Key,E> r, BSTNode<Key,E> t) {
-        super(k, val, l, r);
+    public BSTTNode(Key k, E val, BSTTNode<Key,E> l, BSTTNode<Key,E> r, BSTTNode<Key,E> p, BSTTNode<Key, E> t) {
+        super(k, val, null, null);
+        this.left = l;
+        this.right = r;
         this.thread = t;
-        
+        this.parent = p;
     }
+    
+    public BSTTNode<Key, E> getThread() {
+        return thread;
+    }
+    
+    public void setThread(BSTTNode<Key, E> t) {
+        this.thread = t;
+    }
+    
+    public BSTTNode<Key, E> parent() {
+        return this.parent;
+    }
+    
+    public void setParent(BSTTNode<Key, E> p) {
+        this.parent = p;
+    }
+
+    public BSTTNode<Key, E> left() {
+        return left;
+    }
+
+    public void setLeft(BSTTNode<Key, E> left) {
+        this.left = left;
+    }
+
+    public BSTTNode<Key, E> right() {
+        return right;
+    }
+
+    public void setRight(BSTTNode<Key, E> right) {
+        this.right = right;
+    }
+    
 
 }
